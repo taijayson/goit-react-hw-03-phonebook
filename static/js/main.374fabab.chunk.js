ffee669ed714f7c4c5c7b8795ddd9de59f827b74
Pavel Denisov <denisov.p.p@gmail.com>
2021-04-10 21:12:28 +0300

    Updates

--- static/js/main.374fabab.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{9:function(e,t,o){"use strict";o.r(t);var n=o(1),c=o.n(n),r=o(3),s=o.n(r),i=o(0),a=function(){return Object(i.jsx)("div",{className:"App",children:"Hello"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.374fabab.chunk.js.map
